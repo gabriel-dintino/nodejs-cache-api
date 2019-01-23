ac156871285997b5d98eaf951dedb741e74400e8
Gabriel D'Intino <GDintino@proveedor.teco.com.ar>
2019-01-23 10:26:02 -0300

    Refactoring.

--- routes/redis.cache.route.js
+++ routes/redis.cache.route.js
@@ -1,10 +1,10 @@
 "use strict";
 
+const redisCacheConfiguration = require("../configurations/redis.cache.configuration");
+const API_VERSION_1 = redisCacheConfiguration.getApiVersion1();
+const controller = require("../controllers/redis.cache.controller");
+
 module.exports = (app) => {
-    const redisCacheConfiguration = require("../configurations/redis.cache.configuration");
-    const API_VERSION_1 = redisCacheConfiguration.getApiVersion1();
-    const controller = require("../controllers/redis.cache.controller");
- 
     app.get(`/cache/${API_VERSION_1}/ping`, (req, res) => controller.pong(req, res));
     app.get(`/cache/${API_VERSION_1}/item/:key`, (req, res) => controller.get(req, res));
     app.post(`/cache/${API_VERSION_1}/item`, (req, res) => controller.set(req, res));
